6d254271e7f757baa052330a60eb53d15440d71a
Alexandre Soares <alexandresoares@Alexandres-MacBook-Pro.local>
2021-10-29 09:54:07 +0300

    nuxtjs project

---
+++ components/Hero.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container class="hero">
+    <vue-typed-js :strings="strings" :loop="true" :show-cursor="false">
+      <h1>
+        Hi! I'm a Front End Developer<br />
+        but also <span class="typing"></span>
+      </h1>
+    </vue-typed-js>
+    <v-chip color="primary" class="mt-6 mb-3 pointer"
+      >contact@alexandresoares.fr</v-chip
+    >
+    <p class="d-flex align-center">
+      <span class="green-circle"></span>
+      Looking for remote works
+    </p>
+  </v-container>
+</template>
+
+<script>
+import { VueTypedJs } from 'vue-typed-js'
+export default {
+  components: {
+    VueTypedJs
+  },
+  data() {
+    return {
+      strings: [
+        'a huge fan of games 🎮!',
+        'a cleaning addict!',
+        'a problem solver 🤔!'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+}
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container>
+    <v-row class="justify-space-between my-6">
+      <div>
+        <span>Alexandre <strong>SOARES</strong></span>
+      </div>
+      <div>
+        <v-tooltip bottom>
+          <template #activator="{ on, attrs }">
+            <a target="_blank" href="/CV Alexandre SOARES 2021.pdf">
+              <img
+                src="static/img/icons/emailjs.svg"
+                alt="icon"
+                class="about__icon"
+                v-bind="attrs"
+                v-on="on"
+              />
+            </a>
+          </template>
+          <span>Download my resume</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template #activator="{ on, attrs }">
+            <img
+              src="/img/icons/linkedin.svg"
+              alt="icon"
+              class="about__icon"
+              v-bind="attrs"
+              v-on="on"
+            />
+          </template>
+          <span>Linkedin</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template #activator="{ on, attrs }">
+            <img
+              src="/img/icons/github.svg"
+              alt="icon"
+              class="about__icon"
+              v-bind="attrs"
+              v-on="on"
+            />
+          </template>
+          <span>Github</span>
+        </v-tooltip>
+      </div>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  margin-left: 20px;
+}
+</style>
